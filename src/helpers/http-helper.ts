217777c288e563e9637216c2de7b9da11a20cc82
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-07-07 22:14:46 -0300

    refactor: move server error to a helper method

--- src/helpers/http-helper.ts
+++ src/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from "../presentation/errors/server-error"
 import { HttpResponse } from "../presentation/protocols/http"
 
 export const badRequest = (error: Error): HttpResponse => {
   return {
     statusCode: 400,
     body: error
+  }
+}
+
+export const serverError = (): HttpResponse => {
+  return {
+    statusCode: 500,
+    body: new ServerError()
   }
 }
