21de697ea31c2da4678e5ca85f053eddaaa8e0de
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-07-08 23:03:20 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -17,7 +17,14 @@ describe('EmailValidator Adapter', () => {
 
   test('should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+    const isValid = sut.isValid('valid_email@email.com')
     expect(isValid).toBe(true)
+  })
+
+  test('should rcall validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('valid_email@email.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@email.com')
   })
 })
