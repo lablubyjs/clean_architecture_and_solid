73f8faefee653fe44bcc2738ce9dfc475399e504
Crislâine Santos <criis.santos@outlook.com>
2022-07-11 09:19:43 -0300

    refactor: fix to eslint errors

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,5 @@
-import { EmailValidatorAdapter } from "./email-validator-adapter"
+
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
