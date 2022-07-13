a4aa17c4ce2a5ac2e7b45ed859129c2884fe76b1
Crislâine Santos <criis.santos@outlook.com>
2022-07-13 15:49:57 -0300

    feat: add LogoMongoRepository to SingUpFactory

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -5,6 +5,7 @@ import { BcryptAdapter } from '../../infra/cryptography/brycpt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeSingUpController = (): Controller => {
   const salt = 12
@@ -13,5 +14,6 @@ export const makeSingUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const singUpController = new SingUpController(emailValidator, addAccount)
-  return new LogControllerDecorator(singUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(singUpController, logMongoRepository)
 }
