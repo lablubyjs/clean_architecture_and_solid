557fdba219af8e82b0a6cf66c85d75555125cd09
Crislâine Santos <criis.santos@outlook.com>
2022-07-13 11:11:46 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -3,12 +3,15 @@ import { SingUpController } from '../../presentation/controllers/singup/singup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { BcryptAdapter } from '../../infra/cryptography/brycpt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSingUpController = (): SingUpController => {
+export const makeSingUpController = (): Controller => {
   const salt = 12
   const emailValidator = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SingUpController(emailValidator, addAccount)
+  const singUpController = new SingUpController(emailValidator, addAccount)
+  return new LogControllerDecorator(singUpController)
 }
--- src/presentation/controllers/singup/singup.ts
+++ src/presentation/controllers/singup/singup.ts
@@ -1,12 +1,12 @@
-import { badRequest, serverError, ok } from "../../helpers/http-helper"
-import { MissingParamError, InvalidParamError } from "../../errors"
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from "./singup-protocols"
+import { badRequest, serverError, ok } from '../../helpers/http-helper'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './singup-protocols'
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
 
-  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
   }
@@ -14,27 +14,26 @@ export class SingUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-      for(const field of requiredFields) {
-        if(!httpRequest.body[field]) {
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
       const { name, email, password, passwordConfirmation } = httpRequest.body
-      if(password != passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
       const isValid = this.emailValidator.isValid(email)
-      if(!isValid) {
+      if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
       const account = await this.addAccount.add({
         name,
-        email, 
+        email,
         password
       })
       return ok(account)
     } catch (error) {
-      console.error(error)
       return serverError()
     }
   }
