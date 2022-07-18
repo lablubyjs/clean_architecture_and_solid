13e5b278c1ca15c85c281bb131aa08ef7b24655a
Crislâine Santos <criis.santos@outlook.com>
2022-07-18 12:10:26 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -1,7 +1,7 @@
 import { MissingParamError, ServerError } from '../../errors'
 import { AccountModel, AddAccount, AddAccountModel, HttpRequest, Validation } from './singup-protocols'
 import { SingUpController } from './singup'
-import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
