73e0d5fa47664ff4aa4f149e323a02ceb92e8b64
Crislâine Santos <criis.santos@outlook.com>
2022-07-18 11:10:05 -0300

    fix: correct errors in ValidationComposite

--- src/presentation/helpers/validators/validation-composite.ts
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -1,13 +1,13 @@
-import { Validation } from "./validation";
+import { Validation } from './validation'
 
 export class ValidationComposite implements Validation {
   private readonly validations: Validation[]
 
-  constructor(validations: Validation[]) {
+  constructor (validations: Validation[]) {
     this.validations = validations
   }
 
-  validate(input: any): Error | null {
+  validate (input: any): Error | null {
     for (const validation of this.validations) {
       const error = validation.validate(input)
       if (error) {
