5e115ac5a26d6d2431d6fd84d22146cf0d29a113
Crislâine Santos <criis.santos@outlook.com>
2022-07-19 10:54:46 -0300

    fix: remove console.log and add indentation

--- src/infra/cryptography/jwt-adpater/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adpater/jwt-adapter.ts
@@ -10,7 +10,6 @@ export class JwtAdapter implements Encrypter {
 
   async encrypt (value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret)
-    console.log(accessToken)
     return accessToken
   }
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,7 @@ import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl)
-.then(async () => {
+  .then(async () => {
     const app = await (await import('./config/app')).default
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   })
