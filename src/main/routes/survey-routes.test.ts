5ee3e03acbfdaa300fbe75f1c941a6339b8417f1
Crislâine Santos <criis.santos@outlook.com>
2022-07-21 17:20:13 -0300

    test: add AuthMiddleware on surveys route

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -16,11 +19,13 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
-    test('should return 200 on surveys', async () => {
+    test('should return 403 on add survey without accessToken', async () => {
       await request(app).post('/api/surveys').send({
         question: 'any_question',
         answers: [{
@@ -30,7 +35,37 @@ describe('Survey Routes', () => {
         {
           answer: 'other_answer'
         }]
-      }).expect(204)
+      }).expect(403)
+    })
+
+    test('should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Cris',
+        email: 'cris@email.com',
+        password: 'passwordHash',
+        role: 'admin'
+      })
+      const accessToken = sign({ id: res.insertedId }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: res.insertedId
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          },
+          {
+            answer: 'other_answer'
+          }]
+        }).expect(204)
     })
   })
 })
